8bbb688d490ca07f9cac397ee11f7c5d94e92167
Giovanni Misitano <giovanni.misitano@jyu.fi>
2019-11-05 22:54:37 +0200

    Vectorized most of the functions in BRB, is a lot faster now.

--- desdeo_brb/testbed.py
+++ desdeo_brb/testbed.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 
-def belief_distribution(
-    xs: np.ndarray, hs: np.ndarray
-) -> np.ndarray:
+def belief_distribution(xs: np.ndarray, hs: np.ndarray) -> np.ndarray:
     """Given a set of referential values, transform x into corresponding belief
     distributions.
 
@@ -54,37 +52,79 @@ def belief_distribution(
 
 
 def new_belief_distribution(x, hs):
+    res = np.zeros((x.shape[0],) + hs.shape)
     hs_fwd = np.roll(hs, 1)
     hs_bck = np.roll(hs, -1)
-    zeros = np.zeros((x.shape[0], hs.shape[1]))
+    zeros = np.zeros((x[:, 0].shape[0],) + hs[0].shape)
 
-    max1 = np.fmax((hs_fwd - x) / (hs_fwd - hs), zeros)
-    max1 = np.where((max1 >= 0), max1, 0)
+    for i in range(x.shape[1]):
+        # hs_fwd = np.roll(hs[i], 1)
+        # hs_bck = np.roll(hs[i], -1)
 
-    max2 = np.fmax((x - hs_bck) / (hs - hs_bck), zeros)
-    max2 = np.where((max2 >= 0), max2, 0)
+        # print(x[:, i].reshape(-1, 1))
+        # print(hs_fwd)
+        # a = (hs_fwd - x[:, i].reshape(-1, 1))
+        # b = (hs_fwd - hs[i])
+        # print(np.fmax((a / b), zeros))
 
-    min_term = np.fmin(max1, max2)
-    return np.where(min_term <= 1, min_term, 1)
+        max1 = np.fmax(
+            (hs_fwd[i] - x[:, i].reshape(-1, 1))
+            / (hs_fwd[i] - hs[i].reshape(1, -1)),
+            zeros,
+        )
+        max1 = np.where((max1 >= 0), max1, 0)
+
+        max2 = np.fmax(
+            (x[:, i].reshape(-1, 1) - hs_bck[i]) / (hs[i] - hs_bck[i]), zeros
+        )
+        max2 = np.where((max2 >= 0), max2, 0)
+
+        min_term = np.fmin(max1, max2)
+        res[:, i, :] = np.where(min_term <= 1, min_term, 1)
+
+    return res
+
+
+def cartesian_product(mat) -> np.ndarray:
+        """Takes the element wise cartesian product of the vectors contained in a list.
+
+        Arguments:
+            mat (List[np.ndarray]): A list with 1D numpy arrays used to form
+            the element wise cartesian product.
+
+        Returns:
+            (np.ndarray): The cartesian product as a 2D array. It's dimensions
+            will be (product of the lengths of the vectors contained in mat,
+            length of the longest array in mat)
+
+        """
+        prods = np.zeros((mat.shape[0], mat.shape[2]**mat.shape[1], mat.shape[1]))
+        print(prods.shape)
+
+
+        for i in range(len(mat)):
+            n_referential_sets = len(mat[i])
+            print(n_referential_sets)
+
+            if n_referential_sets == 1:
+                # if math has just one set of referential values, return it
+                prod = mat[i][0].reshape((-1, 1))
+
+            else:
+                grid = np.array(np.meshgrid(*mat[i]))
+                prod = grid.T.reshape(-1, n_referential_sets)
+
+            prods[i] = prod
+
+        return prods
+    
 
 
 if __name__ == "__main__":
-    # 1d case
-    a = np.array([[0.25], [0.27]]).reshape(-1, 1)
-    a = np.random.uniform(0, 0.3, (100000, 1))
+        # 1d case
+    a = np.array([[0.25], [0.33]])
 
     hs = np.array([[0, 0.1, 0.2, 0.3]])
 
-    import timeit
-
-    start = timeit.default_timer()
     res = new_belief_distribution(a, hs)
-    stop = timeit.default_timer()
-    print('Time: ', stop - start)
-
-    start = timeit.default_timer()
-    res_old = np.apply_along_axis(belief_distribution, 1, a, hs)
-    stop = timeit.default_timer()
-    print('Time: ', stop - start)
-
-    # 2d case
+    print(cartesian_product(res))
